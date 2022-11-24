1c96a9a0fdfc2aa54bdfacdf96c7f19316b903fa
brian <kipkoechbrian0@gmail.com>
2022-11-24 10:46:21 +0300

    complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :title_is_clickbait
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+
+    def title_is_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Can't be clickbait")
+        end
+    end
 end
